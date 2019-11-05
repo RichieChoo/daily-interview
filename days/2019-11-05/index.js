9ae305fb36dd9a46920c1d17e3c8252070e05c00
zhucb <zhucb@bsfit.com.cn>
2019-11-05 20:46:30 +0800

    2019-11-05

--- days/2019-11-05/index.js
+++ days/2019-11-05/index.js
@@ -1,6 +1,25 @@
-
 /**
  * @date {Tuesday, Nov 5th 2019}}
  * @author {RichieChoo}
  */
 
+async function async1() {
+	console.log(1);
+	const result = await async2();
+	console.log(3);
+}
+
+async function async2() {
+	console.log(2);
+}
+
+Promise.resolve().then(() => {
+	console.log(4);
+});
+
+setTimeout(() => {
+	console.log(5);
+});
+
+async1();
+console.log(6);
--- days/2019-11-05/index.md
+++ days/2019-11-05/index.md
@@ -1,1 +1,27 @@
 <!-- Tuesday, Nov 5th 2019 -->
+
+## event loop
+
+```
+async function async1() {
+  console.log(1);
+  const result = await async2();
+  console.log(3);
+}
+
+async function async2() {
+  console.log(2);
+}
+
+Promise.resolve().then(() => {
+  console.log(4);
+});
+
+setTimeout(() => {
+  console.log(5);
+});
+
+async1();
+console.log(6);
+
+```
